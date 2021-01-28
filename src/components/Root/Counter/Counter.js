1149dd6c69c8f56a11cb30f0a84e9ee0e3bf6174
Avinash Jain <57763597+logicbomb-1@users.noreply.github.com>
2021-01-28 14:19:43 +0530

    Update Counter.js

--- src/components/Root/Counter/Counter.js
+++ src/components/Root/Counter/Counter.js
@@ -47,7 +47,6 @@ const Counter = () => {
         <Button onClick={() => dispatch(-1)}>-1</Button>
         <Button onClick={() => dispatch(1)}>+1</Button>
         <Button onClick={() => dispatch(2)}>+2</Button>
-        <Button onClick={() => dispatch(5)}>+5</Button>
       </ButtonRow>
     </div>
   );
